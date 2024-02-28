ae3f6858a251f2bff01032f0442d241bfa88d4bd
boramcom <boramcom@daum.net>
2024-02-28 17:51:45 +0900

    스프링부트3(2.5 이후) db설정 변경 사항 추가

--- src/main/resources/import_dummy.sql
+++ src/main/resources/import_dummy.sql
@@ -1,4 +1,4 @@
 DELETE FROM SIMPLE_USERS
-INSERT INTO SIMPLE_USERS VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, '$2a$12$gZ.yvR4/mbCM6ev5L88NOeCh3VeH07K3c5kErn02NwrlsCRyX6fMi', 'ADMIN', 'admin')
-INSERT INTO SIMPLE_USERS VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, '$2a$12$gZ.yvR4/mbCM6ev5L88NOeCh3VeH07K3c5kErn02NwrlsCRyX6fMi', 'USER', 'user')
+INSERT INTO SIMPLE_USERS(id,create_date,modifie_date,enabled,password,role,username) VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, '$2a$12$gZ.yvR4/mbCM6ev5L88NOeCh3VeH07K3c5kErn02NwrlsCRyX6fMi', 'ADMIN', 'admin')
+INSERT INTO SIMPLE_USERS(id,create_date,modifie_date,enabled,password,role,username) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, '$2a$12$gZ.yvR4/mbCM6ev5L88NOeCh3VeH07K3c5kErn02NwrlsCRyX6fMi', 'USER', 'user')
 ALTER SEQUENCE simple_users_id_seq restart with 3
